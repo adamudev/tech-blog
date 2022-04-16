463efd4280ebe2afbf450f92e5a4b64d3a50bacf
Adamu Abubakar <adamudev@gmail.com>
2022-04-16 19:13:39 +0100

    add sitemap

--- next.config.js
+++ next.config.js
@@ -3,3 +3,16 @@ module.exports = {
     domains: ["images.ctfassets.net"],
   },
 };
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/blog": { page: "/blog" },
+      "/blog/": { page: "/blog/" },
+    };
+  },
+};
